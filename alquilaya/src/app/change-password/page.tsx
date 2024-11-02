f1f8b00330bcc726379bb2252da681135e26a99e
SamuelFrancoB4B <141897810+SamuelFrancoB4B@users.noreply.github.com>
2024-11-02 09:33:29 -0500

    sin botn 11

--- alquilaya/src/app/change-password/page.tsx
+++ alquilaya/src/app/change-password/page.tsx
@@ -1,4 +1,4 @@
-/*import ChangePasswordForm from '@/Components/ChangePasswordForm/ChangePassword'
+import ChangePasswordForm from '@/Components/ChangePasswordForm/ChangePassword'
 import Header from '@/Components/Header/Header'
 import { useSearchParams } from 'next/navigation'
 import React from 'react'
@@ -8,15 +8,15 @@ const page = () => {
   return (
     <div>
       <Header/>
-    <div className='container'>
+    {/*<div className='container'>
       <div className='padding-section'>
       <h1 className="pb-12 text-primary">Cambia tu contraseña</h1>
         <ChangePasswordForm/>
       </div>
-    </div>
+    </div>*/}
     </div>
   )
 
 }
 
-export default page*/
+export default page
